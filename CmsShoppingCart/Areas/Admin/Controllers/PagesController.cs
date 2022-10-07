4e168ab450ab85e65dc1b0d503aea7880ba56ccb
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-10-07 21:05:54 +0400

    59 min finished

---
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,26 @@
+﻿using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController : Controller
+    {        
+        private readonly CmsShoppingCartContext _context;
+        public PagesController(CmsShoppingCartContext context)
+        {
+            _context = context;
+        }
+        public async Task <IActionResult> Index()
+        {
+            var pages = _context.Pages.OrderBy(p => p.Sorting);            
+            var pageList = await pages.ToListAsync();                         
+            return View(pageList);
+        }
+    }
+}
