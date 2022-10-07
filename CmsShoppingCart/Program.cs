a23d48d0f1a1c38c5cf5cf0ffdb33bd26fc62794
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-10-07 15:15:45 +0400

    52 min finished

--- CmsShoppingCart/Program.cs
+++ CmsShoppingCart/Program.cs
@@ -1,5 +1,7 @@
+using CmsShoppingCart.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,23 @@ namespace CmsShoppingCart
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
