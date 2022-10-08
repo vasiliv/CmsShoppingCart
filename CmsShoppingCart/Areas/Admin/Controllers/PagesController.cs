e985cb77e6b55aff32b0b1f9864d63acbe6aad6a
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-10-08 22:53:46 +0400

    1h35min finished

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -1,6 +1,7 @@
 ﻿using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,42 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             var pages = _context.Pages.OrderBy(p => p.Sorting);            
             var pageList = await pages.ToListAsync();                         
             return View(pageList);
+        }
+        public async Task<IActionResult> Details (int id)
+        {
+            var page = await _context.Pages.FirstOrDefaultAsync(p => p.Id == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+            return View(page);
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                page.Slug = page.Title.ToLower().Replace(" ", "-");
+                page.Sorting = 100;
+
+                var slug = await _context.Pages.FirstOrDefaultAsync(p => p.Slug == page.Slug);
+                // problem. if slug already exists
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The title already exists");
+                    return View(page);
+                }
+                // good. if slug does not exist
+                _context.Add(page);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return View(page);
         }
     }
 }
